91905bf53c94b624cbab3dfee6923ab6c310ca93
Mahmoud Ramdan <mrd@corelia.ai>
2025-11-29 13:56:46 +0200

    initial project

---
+++ src/main/java/com/mahmoud/nagieb/NagiebApplication.java
@@ -0,0 +1,13 @@
+package com.mahmoud.nagieb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagiebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NagiebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahmoud/nagieb/NagiebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoud.nagieb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagiebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
