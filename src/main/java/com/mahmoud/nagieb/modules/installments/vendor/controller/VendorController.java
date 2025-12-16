b38932d5f815b6ab3f0d1892cb00189db990c9b8
Mahmoud Ramdan <mrd@corelia.ai>
2025-12-16 16:12:22 +0200

    add VendorController with CRUD operations for vendor management

---
+++ src/main/java/com/mahmoud/nagieb/modules/installments/vendor/controller/VendorController.java
@@ -0,0 +1,51 @@
+package com.mahmoud.nagieb.modules.installments.vendor.controller;
+
+import com.mahmoud.nagieb.modules.installments.customer.dto.CustomerSummary;
+import com.mahmoud.nagieb.modules.installments.vendor.dto.VendorRequest;
+import com.mahmoud.nagieb.modules.installments.vendor.dto.VendorResponse;
+import com.mahmoud.nagieb.modules.installments.vendor.dto.VendorSummary;
+import com.mahmoud.nagieb.modules.installments.vendor.service.VendorService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/vendors")
+@AllArgsConstructor
+public class VendorController {
+
+    private final VendorService vendorService;
+
+    @PostMapping
+    public ResponseEntity<VendorSummary> createVendor(@Valid @RequestBody VendorRequest request) {
+        return ResponseEntity.status(201).body(vendorService.create(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<VendorSummary> updateVendor(@PathVariable Long id, @Valid @RequestBody VendorRequest request) {
+
+        return ResponseEntity.ok(vendorService.update(id, request));
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VendorResponse> getVendorById(@PathVariable Long id) {
+        return ResponseEntity.ok(vendorService.getById(id));
+    }
+    @GetMapping
+    public ResponseEntity<Page<VendorSummary>> list(@RequestParam(defaultValue = "0") int page,
+                                    @RequestParam(defaultValue = "10") int size,
+                                    @RequestParam(required = false) String search) {
+        Page<VendorSummary> vendorSummaries = vendorService.list(page, size, search);
+        return ResponseEntity.ok(vendorSummaries);
+    }
+
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<String> delete(@PathVariable Long id) {
+        String message =vendorService.softDelete(id);
+        return ResponseEntity.ok(message);
+    }
+}
+
