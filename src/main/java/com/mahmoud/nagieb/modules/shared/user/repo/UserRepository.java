bf414e13f7f1c23e3c9e0bf491a63173c480e617
Mahmoud Ramdan <mrd@corelia.ai>
2026-02-11 09:15:22 +0200

    uodate user repo and entity

---
+++ src/main/java/com/mahmoud/nagieb/modules/shared/user/repo/UserRepository.java
@@ -0,0 +1,16 @@
+package com.mahmoud.nagieb.modules.shared.user.repo;
+
+import com.mahmoud.nagieb.modules.shared.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
+
